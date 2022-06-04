5d4ffd3f857ab4090c11e72c3cfab6720cd6be8d
Yeren Kalibek <yerenn22@gmail.com>
2022-06-04 17:33:36 +0600

    init: cra & frontendmentor template

---
+++ frontend/src/js/containers/App.js
@@ -0,0 +1,26 @@
+// My balance
+// $921.48
+
+// Spending - Last 7 days
+
+// mon
+// tue
+// wed
+// thu
+// fri
+// sat
+// sun
+
+// Total this month
+// $478.33
+
+// +2.4%
+// from last month
+
+export const App = () => {
+    return (
+        <div className=''>
+            <img src="assets/images/logo.svg" alt="logo" />
+        </div>
+    );
+};
